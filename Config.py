1d730d487e327b192930b8e4748a494c8a2700cb
ONU NNAMDI <onu.nnamdi.2000@gmail.com>
2021-08-06 14:09:28 +0100

    MY FIRST UPLOAD

---
+++ Config.py
@@ -0,0 +1,19 @@
+mobile = '+234 808 579 6361'
+email = 'escet@gmail.com'
+school = 'ESCET'
+
+page_details = {
+    'mobile': mobile ,
+    'email': email,
+    'school': school,
+     'logo': '../static/images/download.jpg',
+}
+menu = {
+    '.' : ['home2','home'],
+    'contact': ['address_book','CONTACT'],
+    'admission': ['library_books','OUR COUSES'],
+    'about': []
+
+}
+
+token =' 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 '
---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template
+from Function import *
+from Config import *
+
+app = Flask(__name__)
+
+app.debug = True
+@app.context_processor
+def details():
+    return dict(pd=page_details, replacer=replacer)
+
+
+@app.route('/about')
+def hello_world():
+    pageinfo = {
+        'title' : 'about us',
+        'sub' : 'college of education enugu (technical) is an instutition built on knowledge and hardwork'
+    }
+    return render_template('about.html', pg=pageinfo)
+
+
+@app.route('/contact')
+def contact():
+    pageinfo = {
+        'title': 'contact',
+        'sub': 'Contact us today to get the best learning experience'
+    }
+    return render_template('contact1.html', pg=pageinfo)
+
+
+@app.route('/hello')
+def index():
+    pageinfo = {
+        'title': 'Welcome',
+
+    }
+    return render_template('index1.html', pg=pageinfo)
+
+@app.route('/courses')
+def courses():
+    pageinfo = {
+        'title': 'courses',
+        'sub': 'so many courses to learn, there is no limit to learning'
+    }
+    return render_template('courses.html', pg=pageinfo)
+
+@app.route('/admission')
+def admission():
+    pageinfo = {
+        'title': 'admission',
+        'sub': 'our addmission is available now'
+    }
+    return render_template('admissions.html',pg=pageinfo)
+
+if __name__ == '__main__':
+    app.run(debug=True)
